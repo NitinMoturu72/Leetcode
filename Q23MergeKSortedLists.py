8a98d62843228fa3e624dc421b27c98d804ef9c9
NitinMoturu72 <nitinmoturu72@gmail.com>
2026-06-22 22:24:17 -0500

    Merge Sort approach

--- Q23MergeKSortedLists.py
+++ Q23MergeKSortedLists.py
@@ -69,3 +69,51 @@ class Solution:
 # The space complexity is O(n) due to the additional list used to store the node values.
 
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if  not lists or len(lists) == 0:
+            return None
+        
+        while len(lists) >1 :
+            mergedLists = []
+            for i in range(0,len(lists), 2):
+                lst1 = lists[i]
+                lst2 = lists[i+1] if i+1 < len(lists) else None
+
+                mergedLists.append(self.mergeLists(lst1,lst2))
+            lists = mergedLists
+        return lists[0]
+
+                
+    def mergeLists(self, list1, list2):
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            
+            tail = tail.next
+        
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
+        
+        return dummy.next
+
+#Merge Sort Approach
+# We can use a divide-and-conquer approach to merge the linked lists.
+# In each iteration, we merge pairs of linked lists until only one remains.
+# The time complexity is O(n log k), where n is the total number of nodes and k is the number of linked lists.
+# The space complexity is O(log k) due to the recursive call stack.
